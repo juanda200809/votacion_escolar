fb0b9687fd67ff42d0552800917b27b187a9a995
caicedoaguirremariaisabella-ops <caicedoaguirremariaisabella-ops@gmail.com>
2026-08-21 18:01:03 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -1,14 +1,28 @@
 <?php
 
-$host = "localhost";
+$host = "127.0.0.1";
 $usuario = "root";
 $password = "";
 $basedatos = "votaciones_escolares";
+$puerto = 3306;
 
-$conn = new mysqli($host, $usuario, $password, $basedatos);
+$conn = new mysqli(
+    $host,
+    $usuario,
+    $password,
+    $basedatos,
+    $puerto
+);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+
+    die(
+        "Error de conexión con MySQL: " .
+        $conn->connect_error
+    );
+
 }
+
+$conn->set_charset("utf8mb4");
 
 ?>
